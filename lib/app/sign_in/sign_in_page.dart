c8384a4c03aa792dfdde400de2ea4bbd5edd6567
hb57786 <tadeufcad@gmail.com>
2021-11-29 18:25:32 -0300

    Adding a Column layout

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -5,7 +5,25 @@ class SignInPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Time Tracker'),
+        title: Text(
+          'Time Tracker',
+          textAlign: TextAlign.center,
+        ),
+        elevation: 2.0,
+      ),
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.orange,
+              child: SizedBox(
+                height: 100.0,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
